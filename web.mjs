703e52e388daeb68849f5dba8786487c1927dcff
Lyumih <Lyumih@users.noreply.github.com>
2023-10-10 07:03:57 +0000

    Deploying to gh-pages from @ Lyumih/money@02ddfae99c16b23f389517561f393c0204361cee 🚀

--- web.mjs
+++ web.mjs
@@ -2880,6 +2880,15 @@ var $;
         dom_name() {
             return "a";
         }
+        uri_off() {
+            return "";
+        }
+        uri_native() {
+            return null;
+        }
+        external() {
+            return false;
+        }
         attr() {
             return {
                 ...super.attr(),
