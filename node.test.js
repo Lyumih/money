4231dfba0b4c1015184d58d7bdfeb00b59b8ab02
Lyumih <Lyumih@users.noreply.github.com>
2024-01-06 07:03:13 +0000

    Deploying to gh-pages from @ Lyumih/money@02ddfae99c16b23f389517561f393c0204361cee 🚀

--- node.test.js
+++ node.test.js
@@ -9593,6 +9593,9 @@ var $;
             uri_base() {
                 return $mol_dom_context.document.location.href;
             }
+            uri_base_abs() {
+                return new URL(this.uri_base(), $mol_dom_context.document.location.href);
+            }
             uri_resolve(uri) {
                 if (/^(\w+script+:)+/.test(uri))
                     return null;
@@ -9607,7 +9610,7 @@ var $;
                     return this.$.$mol_state_arg.link(params);
                 }
                 try {
-                    const url = new URL(uri, this.uri_base());
+                    const url = new URL(uri, this.uri_base_abs());
                     return url.toString();
                 }
                 catch (error) {
@@ -9740,6 +9743,9 @@ var $;
         __decorate([
             $mol_mem_key
         ], $mol_text.prototype, "grid_cell_text", null);
+        __decorate([
+            $mol_mem
+        ], $mol_text.prototype, "uri_base_abs", null);
         __decorate([
             $mol_mem_key
         ], $mol_text.prototype, "uri_resolve", null);
