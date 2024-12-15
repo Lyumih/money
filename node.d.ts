42e93a0f67457d6482b5b1c535e16a777dd4be8f
Lyumih <Lyumih@users.noreply.github.com>
2024-12-15 07:07:55 +0000

    Deploying to gh-pages from @ Lyumih/money@02ddfae99c16b23f389517561f393c0204361cee 🚀

--- node.d.ts
+++ node.d.ts
@@ -2508,7 +2508,7 @@ declare namespace $.$$ {
 declare namespace $ {
     class $mol_syntax2<Lexems extends {
         [name: string]: RegExp;
-    }> {
+    } = {}> {
         lexems: Lexems;
         constructor(lexems: Lexems);
         rules: Array<{
