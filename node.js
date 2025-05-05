4f54bc27f3ee6fe3123cecfb5640023a1b3fa44c
Lyumih <Lyumih@users.noreply.github.com>
2025-05-05 07:10:20 +0000

    Deploying to gh-pages from @ Lyumih/money@02ddfae99c16b23f389517561f393c0204361cee 🚀

--- node.js
+++ node.js
@@ -10398,7 +10398,7 @@ var $;
 			return obj;
 		}
 		type(){
-			return "tel";
+			return "text";
 		}
 		value_string(next){
 			if(next !== undefined) return next;
@@ -10417,6 +10417,7 @@ var $;
 		String(){
 			const obj = new this.$.$mol_string();
 			(obj.type) = () => ((this.type()));
+			(obj.keyboard) = () => ("decimal");
 			(obj.value) = (next) => ((this.value_string(next)));
 			(obj.hint) = () => ((this.hint()));
 			(obj.enabled) = () => ((this.string_enabled()));
--- web.mjs
+++ web.mjs
@@ -9925,7 +9925,7 @@ var $;
 			return obj;
 		}
 		type(){
-			return "tel";
+			return "text";
 		}
 		value_string(next){
 			if(next !== undefined) return next;
@@ -9944,6 +9944,7 @@ var $;
 		String(){
 			const obj = new this.$.$mol_string();
 			(obj.type) = () => ((this.type()));
+			(obj.keyboard) = () => ("decimal");
 			(obj.value) = (next) => ((this.value_string(next)));
 			(obj.hint) = () => ((this.hint()));
 			(obj.enabled) = () => ((this.string_enabled()));
