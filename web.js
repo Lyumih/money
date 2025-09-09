c37d788cc7d83b2889c0e633a96e6b9783b2abec
Lyumih <Lyumih@users.noreply.github.com>
2025-09-09 07:09:45 +0000

    Deploying to gh-pages from @ Lyumih/money@02ddfae99c16b23f389517561f393c0204361cee 🚀

--- web.js
+++ web.js
@@ -8831,6 +8831,7 @@ var $;
 			return {
 				...(super.attr()), 
 				"src": (this.uri()), 
+				"title": (this.hint()), 
 				"alt": (this.title()), 
 				"loading": (this.loading()), 
 				"decoding": (this.decoding()), 
